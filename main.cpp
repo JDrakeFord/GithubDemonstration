7eddff455b93b6267b19b48cade4d5636fc99402
Jesse Ford <52635549+JDrakeFord@users.noreply.github.com>
2021-10-22 12:11:09 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 //HOW TO UPLOAD PROJECT TO GITHUB
 //IT WILL ASK YOU TO LOG INTO GITHUB BEFORE SHARING ON YOUR FIRST REPOSITORY SHARE
 //ONCE YOU MAKE A CHANGE AND WANT TO REUPLOAD (COMMIT AND PUSH), DO THE FOLLOWING
+
+//I am making an edit to show how to pull updates from github to the local project. This is normally done to transfer code from one device to another.
+//You first push from the device with the most up to date code, and then pull from a device that you want to have the code on. I will now
+//save this change and pull it into CLion.
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
