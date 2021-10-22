8c85ce91c02ad29c0b9856845feb74531e075ba1
drake <jdf028@uark.edu>
2021-10-22 12:12:27 -0500

    Added comment - This message will show your changes on github.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 //I am making an edit to show how to pull updates from github to the local project. This is normally done to transfer code from one device to another.
 //You first push from the device with the most up to date code, and then pull from a device that you want to have the code on. I will now
 //save this change and pull it into CLion.
+
+//As you can see, the comments were downloaded from github. I can now make commits from this laptop to reupload changes to github.
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
